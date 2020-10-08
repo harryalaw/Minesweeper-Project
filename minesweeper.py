452099e03c70a9d1198544ea78671f022052ab42
Harry Law <harryalaw@gmail.com>
2020-10-08 11:14:51 +0100

    Changed to using a canvas to display to improve performance

--- minesweeper.py
+++ minesweeper.py
@@ -1,106 +1,14 @@
-from random import randint, sample
+import mineboard
 from tkinter import *
 from PIL import Image, ImageTk
 from math import floor
 import time
 
-
+CELLWIDTH = 26
 # colors = [gray,blue,green,red,purple,turquoise,teal,yellow,pink]
 COLORS = ['#D4D4D4', '#1876E7', '#4BF139', '#E28415', '#B418E7',
           '#68F5AA', '#17D2D3', '#F7EE2B', '#F22BF7']
 DEFAULT_COLOR = '#A0A0A0'
-
-
-class Mineboard:
-    def populateGrid(self):
-        """creates a 2d array containing all the game info"""
-        true_grid = [[' ' for _ in range(self.width)]
-                     for _ in range(self.height)]
-        """initialises mines"""
-        mine_places = sample(range(self.width * self.height), self.minecount)
-        mine_places = [[num // self.width, num % self.width]
-                       for num in mine_places]
-        for loc in mine_places:
-            i, j = loc[0], loc[1]
-            true_grid[i][j] = 'M'
-        """initialises the adjacency numbers"""
-        for i in range(self.height):
-            for j in range(self.width):
-                if true_grid[i][j] == 'M':
-                    continue
-                nbrs = self.get_neighbours(i, j)
-                nbr_mines = 0
-                for nbr in nbrs:
-                    if true_grid[nbr[0]][nbr[1]] == 'M':
-                        nbr_mines += 1
-                true_grid[i][j] = f"{nbr_mines}"
-        return mine_places, true_grid
-
-    def __init__(self, width, height, minecount):
-        self.width = width
-        self.height = height
-        self.minecount = minecount
-        self.mine_places, self.true_grid = self.populateGrid()
-        self.gameboard = [
-            [' ' for _ in range(self.width)] for _ in range(self.height)]
-        self.gamestate = None  # keeps track of win/loss
-        self.flagcount = 0  # keeps track of flags placed on board
-        self.goodflags = 0  # keeps track of correctly placed flags
-        self.revealed_count = 0  # keeps track of total revealed cells
-        self.changes = []   # keeps track of changes made when updating board
-
-    def check_win(self):
-        """checks for game over"""
-        all_seen = self.revealed_count == self.width * self.height - self.minecount
-        if self.gamestate == 'lose':
-            return 'lose'
-        elif all_seen:
-            return 'win'
-
-    def get_neighbours(self, row, col):
-        """returns all neighbours of a cell"""
-        for i in range(max(0, row-1), min(self.height, row+2)):
-            for j in range(max(0, col-1), min(self.width, col+2)):
-                if (i, j) != (row, col):
-                    yield [i, j]
-
-    def reveal_cell(self, row, col):
-        """deals with out of array guesses"""
-        try:
-            """Stops if already revealed"""
-            if self.gameboard[row][col] != ' ':
-                return
-            self.gameboard[row][col] = self.true_grid[row][col]
-            self.changes.append([row, col])
-            self.revealed_count += 1
-            """reveals all adjacent cells to a blank one"""
-            if self.gameboard[row][col] == '0':
-                nbrs = self.get_neighbours(row, col)
-                for nbr in nbrs:
-                    self.reveal_cell(nbr[0], nbr[1])
-            if self.gameboard[row][col] == 'M':
-                self.gamestate = 'lose'
-            self.gamestate = self.check_win()
-        except IndexError:
-            return
-
-    def flag_cell(self, row, col):
-        """places a flag on a given cell"""
-        try:
-            self.changes.append([row, col])
-            if self.gameboard[row][col] == ' ' and self.flagcount < self.minecount:
-                self.gameboard[row][col] = 'F'
-                self.flagcount += 1
-                if self.true_grid[row][col] == 'M':
-                    self.goodflags += 1
-                self.gamestate = self.check_win()
-            elif self.gameboard[row][col] == 'F':
-                self.gameboard[row][col] = ' '
-                self.flagcount -= 1
-                if self.true_grid[row][col] == 'M':
-                    self.goodflags -= 1
-        except IndexError:
-            return
 
 
 class DisplayWindow(Frame):
@@ -112,6 +20,8 @@ class DisplayWindow(Frame):
         self.gridStore = Frame(master)
         # made a mineboard to avoid errors for referencing properties of Mineboard
         self.mineboard = Mineboard(1, 1, 0)
+        self.canvas = Canvas(self.master)
+
         self.create_widgets()
         self.play_again()
 
@@ -214,6 +124,7 @@ class DisplayWindow(Frame):
     def new_game(self, width, height, minecount):
         """Initialises a new game board"""
         self.start_screen.grid_forget()
+        # @TODO Do better entry validation
         try:  # deals with incorrectly filled in entries on start screen
             width, height, minecount = int(width), int(height), int(minecount)
         except ValueError:  # defaults to an easy board
@@ -229,7 +140,7 @@ class DisplayWindow(Frame):
     def play_again(self, row=None, col=None, mine=None):
         """Either returns to the initial screen to change the settings
            or starts a new game with same dimensions"""
-        self.gridStore.grid_forget()
+        self.canvas.place_forget()
         self.scoreboard.grid_forget()
         self.same_again_bttn.grid_forget()
         self.play_again_bttn.grid_forget()
@@ -239,16 +150,20 @@ class DisplayWindow(Frame):
         else:
             self.new_game(row, col, mine)
 
-    def reveal_cell(self, row, col):
+    def reveal_cell(self, event):
         """Reveals a cell on left mouse click"""
+        x = (event.x-2) // CELLWIDTH
+        y = (event.y-2) // CELLWIDTH
         if self.mineboard.gamestate is None:
-            self.mineboard.reveal_cell(row, col)
+            self.mineboard.reveal_cell(y, x)
             self.update_cells()
 
-    def flag_cell(self, row, col):
+    def flag_cell(self, event):
         """Flags a cell on right mouse click"""
         if self.mineboard.gamestate is None:
-            self.mineboard.flag_cell(row, col)
+            x = (event.x-2) // CELLWIDTH
+            y = (event.y-2) // CELLWIDTH
+            self.mineboard.flag_cell(y, x)
             self.update_cells()
             mines_rem = self.mineboard.minecount - self.mineboard.flagcount
             # updates the mines_left label
@@ -264,10 +179,9 @@ class DisplayWindow(Frame):
                 i, j = loc[0], loc[1]
                 if self.mineboard.gameboard[i][j] == 'F':
                     continue
-                self.labels[i][j].config(text='', width=2)
-                self.labels[i][j].config(
-                    image=MINE, borderwidth=0, padx=0, pady=0)
-                self.labels[i][j].pack(fill=BOTH)
+                self.canvas.delete(self.cells[i][j])
+                self.canvas.create_image(
+                    2+j*CELLWIDTH, 2+i*CELLWIDTH, image=MINE, anchor='nw')
 
     def update_cells(self):
         """updates the display based on currently revealed cells"""
@@ -276,27 +190,30 @@ class DisplayWindow(Frame):
         for change in mineboard.changes:
             i, j = change[0], change[1]
             text_val = gameboard[i][j]
-            currLbl = self.labels[i][j]
+
             if text_val == 'M':
-                currLbl.config(text='', width=2)
-                currLbl.config(image=EXPLODED, borderwidth=0, padx=0, pady=0)
-                # clears text and removes padding for image
-                # on separate lines since otherwise the images do not center properly
+                self.canvas.delete(self.cells[i][j])
+                self.cells[i][j] = self.canvas.create_image(
+                    2+j*CELLWIDTH, 2+i*CELLWIDTH, image=EXPLODED, anchor='nw')
                 self.reveal_mines(i, j)
+
             elif text_val == 'F':
-                currLbl.config(text='', width=2)
-                currLbl.config(image=FLAG, borderwidth=0, padx=0,
-                               pady=0)
-                # clears text and removes padding for image
-                # on separate lines else the images do not center properly
+                self.canvas.delete(self.cells[i][j])
+                self.cells[i][j] = self.canvas.create_image(
+                    2+j*CELLWIDTH, 2+i*CELLWIDTH, image=FLAG, anchor='nw')
+
             elif text_val == ' ':
-                currLbl.config(image='', borderwidth=1, padx=1,
-                               pady=1, text=' ', bg=DEFAULT_COLOR, width=2)
-                # resets the cell to original settings
+                self.canvas.delete(self.cells[i][j])
+                self.cells[i][j] = self.canvas.create_rectangle(
+                    2+j*CELLWIDTH, 2+i*CELLWIDTH, (j+1)*CELLWIDTH, (i+1)*CELLWIDTH, fill=DEFAULT_COLOR, outline="")
+
             elif text_val in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
-                currLbl.config(text=text_val, bg=COLORS[int(text_val)])
-                if text_val == '0':
-                    currLbl.config(text=' ')
+                self.canvas.itemconfig(
+                    self.cells[i][j], fill=COLORS[int(text_val)])
+                if text_val != '0':
+                    self.canvas.create_text(
+                        2+j*CELLWIDTH+CELLWIDTH//2, 2+i*CELLWIDTH+CELLWIDTH//2, anchor='center', text=f"{text_val}")
+
         mineboard.changes = []  # removes previous changes
         if mineboard.gamestate is not None:
             # if the game has ended displays game end message and buttons
@@ -306,47 +223,42 @@ class DisplayWindow(Frame):
             self.same_again_bttn.grid(row=4, column=0, columnspan=2)
             self.play_again_bttn.grid(row=4, column=2, columnspan=2)
 
-    """
-  change_display was helped by:
-  https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
-  """
-
     def change_display(self, width, height):
         """creates a new grid and displays it upon new game creation"""
-        self.master.geometry(f"{width*25}x{(height+4)*25}")
-        self.gridStore.destroy()
-        self.gridStore = Frame(self.master)
-        self.gridStore.grid(row=0, column=0, sticky='n')
+        # @TODO Revisit height once we have interaction working
+        # @TODO Add scrollbars incase too big a game is made
+        self.master.geometry(f"{(width)*CELLWIDTH+2}x{(height+5)*CELLWIDTH}")
 
-        self.cells = [[' ' for _ in range(width)] for _ in range(height)]
-        self.labels = [[' ' for _ in range(width)] for _ in range(height)]
-        for i in range(height):
-            for j in range(width):
-                self.cells[i][j] = Frame(
-                    self.gridStore, borderwidth=1, width=24, height=24)
-                currCell = self.cells[i][j]
-                currCell.grid(row=i, column=j, sticky='news')
-                currCell.pack_propagate(0)
-                self.labels[i][j] = Label(
-                    currCell, bg=DEFAULT_COLOR, text=' ', width=2)
-                currLbl = self.labels[i][j]
-                currLbl.pack(fill=BOTH)
-                currLbl.bind("<Button-1>", lambda event, i=i,
-                             j=j: self.reveal_cell(i, j))
-                currLbl.bind("<Button-2>", lambda event,
-                             i=i, j=j: self.flag_cell(i, j))
-                currLbl.bind("<Button-3>", lambda event,
-                             i=i, j=j: self.flag_cell(i, j))
-        for i in range(height):
-            Grid.rowconfigure(self.gridStore, i, weight=1)
-        for j in range(width):
-            Grid.columnconfigure(self.gridStore, j, weight=1)
+        # figure out dimensions of canvas
+        self.canvas = Canvas(self.master, width=CELLWIDTH*(width+1)+2,
+                             height=CELLWIDTH*(height+1)+2, borderwidth=0, highlightthickness=0)
+        self.cells = []
+
+        for y in range(height):
+            row = []
+            for x in range(width):
+                # drawing the cells with a 2 pixel padding and width of 25
+                row.append(self.canvas.create_rectangle(
+                    2+x*CELLWIDTH, 2+y*CELLWIDTH, (x+1)*CELLWIDTH, (y+1)*CELLWIDTH, fill=DEFAULT_COLOR, outline=""))
+            self.cells.append(row)
+
+        self.canvas.place(relx=0, rely=0, anchor='nw')
+        self.canvas.bind("<Button-1>", self.reveal_cell)
+        self.canvas.bind("<Button-2>", self.flag_cell)
+        self.canvas.bind("<Button-3>", self.flag_cell)
+
+        self.canvas.focus_set()
 
 
 ##################################################
-# creates a window
-root = Tk()
-root.title("Minesweeper")
-app = DisplayWindow(root)
-root.mainloop()
+def main():
+    # creates a window
+    root = Tk()
+    root.title("Minesweeper")
+    app = DisplayWindow(root)
+    root.mainloop()
 ###################################################
+
+
+if __name__ == "__main__":
+    main()
